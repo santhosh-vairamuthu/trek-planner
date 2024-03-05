3821144396034b19979e2dcfcec6c7c2d4631bc9
santhosh <santhosh22032004@gmail.com>
2024-03-05 16:32:16 +0530

    home init

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,13 +1,9 @@
-import logo from "./assets/logo.png";
+
 
 const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 Footer" >
-            <div className="mt-3 container row">
-                <p className="col-8 d-inline-flex justify-content-start" px-5>
-                    <img src={logo} width="30" height="30" className="d-inline-block align-top"alt="Logo"/>
-                    <span className='trekTitle'> Trek Planner</span>
-                </p>
+            <div className="mt-3 container ">
                 <span className="col-4 d-inline-flex">© 2024 Trek planner</span>
             </div>
         </footer>
